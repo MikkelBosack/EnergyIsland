bb9ba3b9d008a7cbaea8fc8031eeb9daa493ed15
Mikkel Bosack Simonsen <bosack@gmail.com>
2021-09-07 12:07:05 +0200

    Added files

---
+++ BEDB-Denmark/CopySNTfileToStandardVEDAfolder.bat
@@ -0,0 +1,20 @@
+@echo off
+echo .
+echo .
+
+echo This file will copy the new SNT-file from this GIT folder to the VEDA-Backend standard location
+echo .
+echo               C:\VEDA\VEDA_BE\Databases\Denmark\Veda_SnT.mdb
+echo .
+echo .
+echo .
+
+pause
+echo .
+echo .
+
+copy Veda_SnT.mdb c:\VEDA\VEDA_BE\Databases\Denmark\Veda_SnT.mdb
+echo .
+echo .
+
+pause
